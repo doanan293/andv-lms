b097e5c941fdfbd135c2ead3d0f851ec6dfce705
Doan Van An <andv@vcsvietnam.vn>
2025-12-13 12:41:32 +0700

    up

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,7 +1,25 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+} from "@/components/ui/card";
+import { GithubIcon } from "lucide-react";
+
 export default function LoginPage() {
   return (
-    <div>
-      <h1>Login Page</h1>
-    </div>
+    <Card>
+      <CardHeader className="text-xl">
+        Welcome back
+        <CardDescription>Login with your Github Email</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <Button>
+          <GithubIcon className="size-4" />
+          Sign in with Github
+        </Button>
+      </CardContent>
+    </Card>
   );
 }
