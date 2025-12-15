397664a3bcc56cd3cbf645a0eb860163cc386275
Doan Van An <andv@vcsvietnam.vn>
2025-12-15 16:11:17 +0700

    up

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
   return (
     <div>
       <h1>Hello World</h1>
