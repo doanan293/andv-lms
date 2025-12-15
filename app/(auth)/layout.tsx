86b3385292a7b63eccbad866c65f718ad9d0267e
Doan Van An <andv@vcsvietnam.vn>
2025-12-15 11:49:08 +0700

    up db

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,3 +1,8 @@
+import { buttonVariants } from "@/components/ui/button";
+import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
+import Image from "next/image";
+import Logo from "@/public/logo.svg";
 export default function AuthLayout({
   children,
 }: {
@@ -5,7 +10,37 @@ export default function AuthLayout({
 }) {
   return (
     <div className="relative flex min-h-svh flex-col items-center justify-center">
-      <div className="flex w-full max-w-sm flex-col gap-6">{children}</div>;
+      <Link
+        href="/"
+        className={buttonVariants({
+          variant: "outline",
+          className: "absolute top-4 left-4",
+        })}
+      >
+        <ArrowLeft className="size-4" />
+        Back
+      </Link>
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <Link
+          href="/"
+          className="flex items-center gap-2 self-center font-medium"
+        >
+          <Image src={Logo} alt="Logo" width={32} height={32} />
+          AndvLMS
+        </Link>
+        {children}
+        <div className="text-balance text-center text-xs text-muted-foreground">
+          By clicking continue, you agree to our{" "}
+          <span className="hover:text-primary hover:underline">
+            Terms of service
+          </span>{" "}
+          and{" "}
+          <span className="hover:text-primary hover:underline">
+            Privacy Policy
+          </span>
+          .
+        </div>
+      </div>
     </div>
   );
 }
